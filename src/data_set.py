118c388e550583d9adc34dd5955d6c7b9d21e53e
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-03-10 19:32:34 +0200

    Started features extraction implementation

--- src/data_set.py
+++ src/data_set.py
@@ -3,8 +3,7 @@
 """
 The data set loader. It will create numeric features matrix by chunking Noun 
 Phrases (NP) from provided data corpus and substituting words in the NP with
-corresponding indices from Glove vectors. The feature spaces we'll try to use
-as described in: ftp://ling.upenn.edu/papers/students/nrh/han-chodorow04.pdf
+corresponding indices from Glove vectors.
 
 @author: yaric
 """
@@ -12,10 +11,36 @@ as described in: ftp://ling.upenn.edu/papers/students/nrh/han-chodorow04.pdf
 import json
 import numpy as np
 
+import tree_dict as td
+
 def __read_json(file):
+    """
+    Loads JSON data from file
+    Arguments:
+        file: the path to the JSON file
+    Return:
+        the dictionary with parsed JSON
+    """
     with open(file) as f:
         data = json.load(f)
     return data
+
+def extractFeatures(node, features, row, glove):
+    """
+    Method to extract features from provided node and store it in features array
+    Arguments:
+        node: the parse tree node with sentence
+        features: the ndarray to store features row
+        row: the row at features array to hold data
+        glove: the glove indices map
+    """
+    """
+    Features map:
+    DT index | NN(S) index | types between DT and NN | 
+    CC | CD	| DT	| EX	 | FW | IN | JJ | JJR | JJS| LS | MD | NN | NNS | NNP | NNPS | PDT	| POS | PRP | PRP$ | RB | RBR | RBS | RP | SYM
+    TO | UH | VB | VBD | VBG | VBN | VBP | VBZ | WDT | WP | WP$ | WRB
+    """
+    
 
 def __create(corpus_file, parse_tree_file, glove_file, corrections_file, test = False):
     """
@@ -31,14 +56,36 @@ def __create(corpus_file, parse_tree_file, glove_file, corrections_file, test = 
         wil be None
     """
     #text_data = __read_json(corpus_file)
-    #corrections = __read_json(corrections_file)
-    parse_tree_list = __read_json(parse_tree_file)
+    corrections = __read_json(corrections_file)
+    parse_trees_list = __read_json(parse_tree_file)
     #glove_indices = __read_json(glove_file)
     
-    # iterate over constituency parse trees and extract NP
+    # The sanity checks
+    if len(corrections) != len(parse_trees_list):
+        raise("Corrections list lenght: %d, not equals parse trees count: %d" 
+              % (len(corrections), len(parse_trees_list)))
+    
+    # iterate over constituency parse trees and extract features
     index = 0
-    for subtree in parse_tree_list:
-        print(subtree)
+    corrected_sentences = 0
+    for tree_dict in parse_trees_list:
+        # get corrections list for the sentence
+        s_corr = corrections[index]
+        # get parse tree for sentence
+        tree, _ = td.treeFromDict(tree_dict)
+        # do sanity check
+        leaves = tree.leaves()
+        if len(s_corr) != len(leaves):
+            raise Exception("Corrections list lenght: %d not equal tree leaves count: %d at index: %d"  % (len(s_corr), len(leaves), index))
+        # check if sentence has corrections
+        hasCorrections = any(w != None for w in s_corr)
+        if hasCorrections:
+            corrected_sentences += 1
+        
+        index += 1
+        
+        
+    print("Total senteces: %d, sentences with correction: %d" % (len(corrections), corrected_sentences))
         
         
 if __name__ == '__main__':
