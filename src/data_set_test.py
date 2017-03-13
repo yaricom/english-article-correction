7cf7fc6adef057bbcdb169d94946b3d8a37fdd50
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-03-13 18:48:30 +0200

    Results generator methods moved to separate scripts

--- src/data_set_test.py
+++ src/data_set_test.py
@@ -8,9 +8,6 @@ The test cases for data set implementation
 
 import unittest
 import numpy as np
-import os
-import json
-import itertools
 
 import utils
 import config
@@ -109,78 +106,6 @@ class TestDataSetMethods(unittest.TestCase):
         self.assertGreater(features.shape[0], 0, "Empty features returned")
         self.assertEqual(features.shape[1], ds.n_features,
                           "Wrong feature dimensions: %d" % features.shape[1])
-
-    def test_save_predictions(self):
-        predictions = np.array(
-                [
-                        [[0,1], [0,1], [ds.DT.A, 0.6],[0,1], [0,1],[0,1], [0,1], [ds.DT.THE, 0.8]],
-                        [[0,1], [ds.DT.AN, 0.6],[0,1], [0,1], [0,1], [ds.DT.A, 0.8],[0,1], [0,1]],
-                        [[0,1], [0,1],[0,1], [0,1], [0,1], [ds.DT.A, 0.8],[0,1], [0,1]]
-                ], dtype = "f")
-        file = config.unit_tests_dir + "/save_predictions_test.txt"
-        if os.path.exists(config.unit_tests_dir) == False:
-            os.makedirs(config.unit_tests_dir)
-        
-        ds.savePredictions(predictions, file)
-        
-        # test saved predictions
-        saved = utils.read_json(file)
-        self.assertIsNotNone(saved, "Failed to save predictions")
-        self.assertEqual(len(predictions), len(saved), "Wrong size of saved sentences")
-        
-        for i in range(len(predictions)):
-            pr = predictions[i]
-            s_pr = saved[i]
-            self.assertEqual(len(pr), len(s_pr), 
-                             "Wrong senetence length [%d] in saved predictions at index: %d" 
-                             % (len(s_pr), i))
-            for j in range(len(pr)):
-                if pr[j][0] == 0:
-                    self.assertIsNone(s_pr[j], 
-                                      "Wrong saved None prediction class at: %d, %d" % (i, j))
-                else:
-                    art = ds.DT.nameByValue(pr[j][0]).lower()
-                    self.assertEqual(art, s_pr[j][0],
-                                     "Wrong article prediction class at: %d, %d" % (i, j))
-                    self.assertAlmostEqual(pr[j][1], s_pr[j][1], places = 2,
-                                     msg = "Wrong article confidence value at: %d, %d" % (i, j))
-                
-
-    def test_predictions_for_sentence(self):    
-        labels = np.array([[ 0.9       ,  0.1       ,  0.        ,  0.        ],
-                           [ 0.55      ,  0.45      ,  0.        ,  0.        ],
-                           [ 0.        ,  0.        ,  0.        ,  1.        ]], dtype = 'f')
-    
-        text_data = json.loads('[["Silly", "libs", "--", "the", "government", "owns", "the", "uterus", "of", "all", "women", "."], \
-                                ["It", "is", "in", "a", "Constitution", "!"]]')
-        parse_trees_list = json.loads('[{"name": "TOP", "children": [{"name": "S", "children": [{"name": "NP", "children": [{"name": "JJ", "children": [{"name": "Silly", "children": []}]}, {"name": "NNS", "children": [{"name": "libs", "children": []}]}]}, {"name": ":", "children": [{"name": "--", "children": []}]}, {"name": "NP", "children": [{"name": "DT", "children": [{"name": "the", "children": []}]}, {"name": "NN", "children": [{"name": "government", "children": []}]}]}, {"name": "VP", "children": [{"name": "VBZ", "children": [{"name": "owns", "children": []}]}, {"name": "NP", "children": [{"name": "NP", "children": [{"name": "DT", "children": [{"name": "the", "children": []}]}, {"name": "NN", "children": [{"name": "uterus", "children": []}]}]}, {"name": "PP", "children": [{"name": "IN", "children": [{"name": "of", "children": []}]}, {"name": "NP", "children": [{"name": "DT", "children": [{"name": "all", "children": []}]}, {"name": "NNS", "children": [{"name": "women", "children": []}]}]}]}]}]}, {"name": ".", "children": [{"name": ".", "children": []}]}]}]}, \
-                                        {"name": "TOP", "children": [{"name": "S", "children": [{"name": "NP", "children": [{"name": "PRP", "children": [{"name": "It", "children": []}]}]}, {"name": "VP", "children": [{"name": "VBZ", "children": [{"name": "is", "children": []}]}, {"name": "PP", "children": [{"name": "IN", "children": [{"name": "in", "children": []}]}, {"name": "NP", "children": [{"name": "DT", "children": [{"name": "the", "children": []}]}, {"name": "NNP", "children": [{"name": "Constitution", "children": []}]}]}]}]}, {"name": ".", "children": [{"name": "!", "children": []}]}]}]}]')
-        
-        test_list = [[None, None, None, [0, 0.9], None, None, [0, 0.55], None, None, None, None, None], 
-                     [None, None, None, [3, 1.0], None, None]]
-        test_list = itertools.chain(*test_list)
-
-        out_list = list()
-        l_index = 0    
-        for i in range(len(parse_trees_list)):
-            sentence = text_data[i]
-            node, _ = td.treeFromDict(parse_trees_list[i]) # the parse tree for sentence
-            dpa_nodes = node.dpaSubtrees()
-            s_list = ds.predictionsForSentence(sentence, labels[l_index : l_index + len(dpa_nodes),], dpa_nodes)
-            out_list.append(s_list)
-            # move to the next sentence labels
-            l_index += len(dpa_nodes)
-    
-        out_list = itertools.chain(*out_list)
-
-        # compare results with test
-        for test, res in zip(test_list, out_list):
-            if test != None:
-                self.assertEqual(test[0], res[0], "Not equal class labels: %s != %s" % (test[0], res[0]))
-                self.assertAlmostEqual(test[1], res[1], 5, "Not equal confidence values: %s != %s" % (test[1], res[1]))
-            else:
-                self.assertEqual(test, res, "Not equal items: %s != %s" % (test, res))
-
         
 if __name__ == '__main__':
     unittest.main()
