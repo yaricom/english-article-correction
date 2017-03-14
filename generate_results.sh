0076421cd8827b462bfa204e9f608c292af3ca95
Iaroslav Omelianenko <yaric.mail@gmail.com>
2017-03-14 23:15:49 +0200

    Included data set creation

--- generate_results.sh
+++ generate_results.sh
@@ -5,8 +5,13 @@
 
 cd src
 
+# Create data set files
+/usr/bin/env python3 data_set.py train --f_type tags
+/usr/bin/env python3 data_set.py validate --f_type tags
+/usr/bin/env python3 data_set.py test --f_type tags
+
 # Do prediction with RandomForest
 /usr/bin/env python3 predictor.py RandomForest --save_model --validate_model --save_labels
 
 # Generate and save submission results
-/usr/bin/env python3 results_generator.py
+/usr/bin/env python3 results_generator.py --f_type tags
